a31bbf6d8e89cd03a61f184b9059905501b255e3
Hanane Moshe <hananemo@gmail.com>
2016-11-10 17:12:38 -0500

    Added Unit Test - and test method for resting api request

---
+++ BrightLocal/BrightLocalTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrightLocal;
+using Newtonsoft.Json;
+[TestClass]
+public class BrightLocalTests
+{
+    [TestMethod]
+    public void apicreateTest()
+    {
+        // Arrange
+
+        api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+        var parameters = new api.Parameters();
+        parameters.Add("name", "Le Bernardin");
+        parameters.Add("company-url", "http://www.example.com");
+        parameters.Add("business-category-id", "791");
+
+        // execute the request
+        var success = request.Post("v1/clients-and-locations/clients/", parameters);
+      
+        //Verify
+        Assert.AreEqual(HttpStatusCode.OK, success.StatusCode);
+        Assert.IsTrue(success.ContentLength > 0);
+        
+
+    }
+}
---
+++ BrightLocal/test/BrightLocalTests/ApiTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrightLocal;
+using RestSharp;
+using System.Net;
+
+namespace BrightLocalTests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        [TestMethod]
+        public void testRequest()
+        {
+            //arrange
+            api request = new api("1a08b2e1fd07fa4150f91b80636906a9a29b8e47", "57645cb550c37");
+            
+            var parameters = new api.Parameters();
+            parameters.Add("name", "Le Bernardin");
+            parameters.Add("company-url", "http://www.example.com");
+            parameters.Add("business-category-id", "791");
+
+            //act
+            var success = request.Post("v1/clients-and-locations/clients/", parameters);
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, success.StatusCode);
+            
+        }
+
+        [TestMethod]
+        public void testCreateExpires()
+        {
+            //arrange
+            api request = new api("1a08b2e1fd07fa4150f91b80636906a9a29b8e47", "57645cb550c37");
+
+            var parameters = new api.Parameters();
+            parameters.Add("name", "Le Bernardin");
+            parameters.Add("company-url", "http://www.example.com");
+            parameters.Add("business-category-id", "791");
+
+            //act
+            var success = request.Post("v1/clients-and-locations/clients/", parameters);
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, success.StatusCode);
+
+        }
+
+    }
+}
