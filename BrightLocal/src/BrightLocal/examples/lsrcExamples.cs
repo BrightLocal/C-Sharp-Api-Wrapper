ad3b4cc58838060d7cbcca6570c8b41afc9e2c31
Hanane Moshe <hananemo@gmail.com>
2016-11-11 08:28:59 -0500

    Added examples for lsrc
    Both in examples class and in git docs

---
+++ BrightLocal/src/BrightLocal/examples/lsrcExamples.cs
@@ -0,0 +1,111 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrightLocal.examples
+{
+    public class lsrcExamples
+    {
+        public static IRestResponse addReport()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("name", "Le Bernardin");            
+            parameters.Add("schedule", "Adhoc");
+            parameters.Add("search-terms", "Restaurant\nfood+nyc\ndelivery+midtown+manhattan");
+            parameters.Add("website-addresses", ["le-bernardin.com", "le-bernardin2.com"]);
+            parameters.Add("search-engines", "google, google-mobile, google-local, yahoo, yahoo-local, bing, bing-local");
+            var success = request.Post("v2/lsrc/add", parameters);
+
+            return success;
+        }
+
+        public static IRestResponse updateReport()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9907");
+            parameters.Add("name", "Le Bernardin");
+            parameters.Add("schedule", "Adhoc");
+            parameters.Add("search-terms", "Restaurant\nfood+nyc\ndelivery+midtown+manhattan");
+            parameters.Add("website-addresses", ["le-bernardin.com", "le-bernardin2.com"]);
+            parameters.Add("search-engines", "google, google-mobile, google-local, yahoo, yahoo-local, bing, bing-local");
+            var success = request.Post("v2/lsrc/add", parameters);
+
+            return success;
+        }
+
+        public static IRestResponse deleteReport()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9907");            
+            var success = request.Delete("v2/lsrc/delete", parameters);
+
+            return success;
+        }
+
+        public static IRestResponse getAllReports()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();            
+            var results = request.Get("v2/lsrc/get-all", parameters);
+
+            return results;
+        }
+
+        public static IRestResponse getReport()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");
+
+            var results = request.Get("v2/lsrc/get", parameters);
+
+            return results;
+        }
+
+        public static IRestResponse runReport()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");
+
+            var success = request.Post("v2/lsrc/run", parameters);
+
+            return success;
+        }
+
+        public static IRestResponse getReportHistory()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");
+
+            var results = request.Get("/v2/lsrc/history/get", parameters);
+
+            return results;
+        }
+
+        public static IRestResponse getReportResults()
+        {
+            api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+            var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9636");
+
+            var results = request.Get("v2/lsrc/results/get", parameters);
+
+            return results;
+        }
+    }
+}
---
+++ LSRC.md
@@ -0,0 +1,98 @@
+Local Search Rank Checker
+-----
+
+### Add Report
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("name", "Le Bernardin");            
+            parameters.Add("schedule", "Adhoc");
+            parameters.Add("search-terms", "Restaurant\nfood+nyc\ndelivery+midtown+manhattan");
+            parameters.Add("website-addresses", ["le-bernardin.com", "le-bernardin2.com"]);
+            parameters.Add("search-engines", "google, google-mobile, google-local, yahoo, yahoo-local, bing, bing-local");
+
+    var success = request.Post("v2/lsrc/add", parameters);    
+```
+
+### Update Report
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9907");
+            parameters.Add("name", "Le Bernardin");
+            parameters.Add("schedule", "Adhoc");
+            parameters.Add("search-terms", "Restaurant\nfood+nyc\ndelivery+midtown+manhattan");
+            parameters.Add("website-addresses", ["le-bernardin.com", "le-bernardin2.com"]);
+            parameters.Add("search-engines", "google, google-mobile, google-local, yahoo, yahoo-local, bing, bing-local");
+
+    var success = request.Post("v2/lsrc/update", parameters);    
+```
+
+### Delete Report
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9907");            
+
+    var success = request.Delete("v2/lsrc/delete", parameters);    
+```
+
+### Get All Reports
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();            
+    
+    var results = request.Get("v2/lsrc/get-all", parameters); 
+```
+
+### Get Report
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");    
+    
+    var results = request.Get("v2/lsrc/get", parameters); 
+```
+
+### Run Report
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");
+
+    var success = request.Post("v2/lsrc/run", parameters);
+```
+
+### Get Report History
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "50");    
+    
+    var results = request.Get("/v2/lsrc/history/get", parameters); 
+```
+
+### Get Report Results
+
+```csharp
+    api request = new api("<INSERT_API_KEY>", "<INSERT_API_SECRET>");
+
+    var parameters = new api.Parameters();
+            parameters.Add("campaign-id", "9636");    
+    
+    var results = request.Get("v2/lsrc/results/get", parameters); 
+```
