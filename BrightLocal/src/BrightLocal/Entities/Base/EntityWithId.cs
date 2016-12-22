1a53fa735ecce89cb85394b7c1846d31d37e0570
Hanane Moshe <hananemo@gmail.com>
2016-12-22 15:39:32 -0700

    added base class containing Id property

---
+++ BrightLocal/src/BrightLocal/Entities/Base/EntityWithId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrightLocal.Entities.Base
+{
+    public class EntityWithId
+    {
+        public string id { get; set; }
+    }
+}
