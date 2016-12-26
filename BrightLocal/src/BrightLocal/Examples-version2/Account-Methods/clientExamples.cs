b549ad733e21e4b973e278abdeedc13ab5e7821b
Hanane Moshe <hananemo@gmail.com>
2016-12-26 10:42:12 -0700

    Added example for creating a client

---
+++ BrightLocal/src/BrightLocal/Examples-version2/Account-Methods/clientExamples.cs
@@ -0,0 +1,24 @@
+﻿using BrightLocal.Entities;
+using BrightLocal.Services;
+using BrightLocal.Services.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrightLocal.Examples_version2.Account_Methods
+{
+    public class clientExamples
+    {
+        public static BrightLocalClient Create()
+        {
+            var myClient = new ClientOptions();
+            myClient.name = "Le Bernardin";
+            myClient.companyUrl = "le-bernardin.com";
+            myClient.businessCategoryId = 791;
+
+            var clientService = new ClientService();
+            return clientService.Create(myClient);
+        }
+    }
+}
