1cb54002aa22df77ad096cc7b3dccfa2cfadbaf0
Hanane Moshe <hananemo@gmail.com>
2017-01-04 10:29:03 -0700

    added json properties to entity

--- BrightLocal/src/BrightLocal/Entities/BrightLocalLscuSearch.cs
+++ BrightLocal/src/BrightLocal/Entities/BrightLocalLscuSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,18 +8,27 @@ namespace BrightLocal
 {
     public class BrightLocalLscuSearch
     {
+        [JsonProperty("success")]
         public bool success { get; set; }
+        [JsonProperty("reports")]
         public List<Report> reports { get; set; }
     }
 
     public class Report
     {
+        [JsonProperty("report_id")]
         public string report_id { get; set; }
+        [JsonProperty("report_name")]
         public string report_name { get; set; }
+        [JsonProperty("location_id")]
         public string location_id { get; set; }
+        [JsonProperty("date_added")]
         public string date_added { get; set; }
+        [JsonProperty("last_run_date")]
         public string last_run_date { get; set; }
+        [JsonProperty("last_message")]
         public string last_message { get; set; }
+        [JsonProperty("currently_running")]
         public string currently_running { get; set; }
     }
 }
